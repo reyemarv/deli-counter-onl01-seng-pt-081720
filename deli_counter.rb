cd457dfd14d29e0234e12e08cf616a3926319f26
Marvin Reyes <reyemarv@gmail.com>
2020-11-23 06:33:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(katz_deli)
   if katz_deli.length > 0 
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1){|customer| currnt_line << "customer"}
-  else puts "The Line is currently empty."
+    katz_deli.each.with_index(1){|customer| current_line << "customer"}
+  else puts "The line is currently empty."
   end 
 end 
