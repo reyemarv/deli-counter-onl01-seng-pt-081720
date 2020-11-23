4db434f22f07f5ead43aa00489fdad6e3ee33d11
Marvin Reyes <reyemarv@gmail.com>
2020-11-23 06:32:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(katz_deli)
   if katz_deli.length > 0 
-    
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1){|customer| currnt_line << "customer"}
   else puts "The Line is currently empty."
   end 
 end 
